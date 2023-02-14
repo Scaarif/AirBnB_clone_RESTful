ea500ec720d52cfe1582ba12668eee4be71dd602
scaarifintl <scaarifintl@gmail.com>
2023-02-14 21:06:24 +0300

    task 5: create a handler for 404 errors that returns a JSON-formatted 404 status code response

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """ Instantiates a Flask application & registers BluePrints """
 from api.v1.views import app_views
-from flask import Flask
+from flask import Flask, make_response, jsonify
 from models import storage
 import os
 
@@ -11,13 +11,18 @@ app = Flask(__name__)
 # register app_views Blueprint (include url_prefix)
 app.register_blueprint(app_views, url_prefix='/api/v1/')
 
-# remove current SQLAlchemy Session after each request (by closing it)
-
 
 @app.teardown_appcontext
 def close_context(self):
     """ tears down current session """
     storage.close()
+
+
+@app.errorhandler(404)
+def not_found(error):
+    """ Creates a handler for 404 errors that returns a JSON-formatted
+    404 status code response """
+    return make_response(jsonify({'error': 'Not found'}), 404)
 
 
 if __name__ == '__main__':
